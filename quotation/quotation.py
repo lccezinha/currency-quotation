e634d14980e582d123f87e3f6b153aecd5455f52
Luiz Cezer <lccezinha@gmail.com>
2021-01-04 12:36:47 -0300

    WIP

--- quotation/quotation.py
+++ quotation/quotation.py
@@ -1,2 +1,7 @@
-def name(name):
-    return f"Hello {name}"
+from typing import string
+
+class Quotation:
+    available_currencies = ["USD", "BRL"]
+
+    def __init__(from, to, amount=0.0) -> string:
+        return ""
