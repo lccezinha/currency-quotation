6a6e2b1ce96bc9c87e3a9e43f442aa76ee959cf9
Luiz Cezer <lccezinha@gmail.com>
2021-01-04 15:10:00 -0300

    Add tests to assign variable values

---
+++ lib/quotation/quotation.py
@@ -0,0 +1,23 @@
+class Quotation:
+    available_currencies = ["USD", "BRL"]
+
+
+    def __init__(self, currency_from, currency_to, amount=0.0) -> str:
+        if not type(currency_from) is str:
+            raise AttributeError("currency_from must be a string")
+
+        if not type(currency_to) is str:
+            raise AttributeError("currency_to must be a string")
+
+        if not currency_from:
+            raise AttributeError("currency_from must not be blank")
+
+        if not currency_to:
+            raise AttributeError("currency_to must not be blank")
+
+        if amount < 0:
+            raise AttributeError("amount must not be negative")
+
+        self.currency_from = currency_from
+        self.currency_to = currency_to
+        self.amount = amount
--- quotation/quotation.py
+++
@@ -1,7 +0,0 @@
-from typing import string
-
-class Quotation:
-    available_currencies = ["USD", "BRL"]
-
-    def __init__(from, to, amount=0.0) -> string:
-        return ""
